918b36b4e67a9a2d4528a1232215403f344005ce
sevinc995 <sevincseferova25@gmail.com>
2025-04-17 18:31:47 +0000

    jshtml

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
-$(document).ready(function(){
-    $(document).on("keypress", function(e){
+$(document).ready(function () {
+    $(document).on("keypress", function(e) {
         if (e.which == 13) {
-            let url = ``
+            let url = `https://apidemo-phi.vercel.app/api/news`;
+            async function mynews() {
+                let responce = await fetch(url);
+                let data = await responce.json();
+                console.log(data);
+            }
+            mynews();
         }
-    })
-})
+    });
+});
